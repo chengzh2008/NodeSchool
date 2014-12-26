7a85f402a7036af6f2bba6d88cd773f8394c070e
chengzh2008 <chengzh2008@gmail.com>
2014-12-25 23:08:38 -0800

    functional javascript upto challenge 4

---
+++ functionaljavascript/tutorial3.js
@@ -0,0 +1,7 @@
+function doubleAll(numbers) {
+    // SOLUTION GOES HERE
+    return numbers.map(function(item){
+        return 2*item;
+    })
+}
+module.exports = doubleAll
---
+++ functionaljavascript/tutorial4.js
@@ -0,0 +1,8 @@
+function getShortMesages(messages) {
+    return messages.filter(function(message){
+        return message.message.length <50;
+    }).map(function(message){
+        return message.message;
+    })
+}
+module.exports = getShortMesages;
